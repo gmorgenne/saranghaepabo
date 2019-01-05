eef39e2741ea4563b341443aebdaaee0d7d7884c
Geoff <gmorgenne@gmail.com>
2019-01-05 17:09:23 -0600

    Initial php test
    adding and testing first php files after php and apache config

---
+++ src/index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<meta>
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link>
+	<title>Home -- Saranghae! (pabo)</title>
+	<script type=""></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<style></style>
+</head>
+<body>
+	<header>
+		<div class="header__container">
+			<h1 class="site-title">
+				<a href="http://www.saranghaepabo.com/" title="Saranghae! (pabo)">Saranghae! (pabo)</a>
+			</h1>
+			<h2 class="site-description">A home for Korean Pop Culture</h2>
+		</div><!-- end of header__container -->
+		<?php
+			include './views/_primary-navigation.php';
+		?>
+
+
+	</header>
+	<div class="wrapper">
+		
+		<main>
+			<div class="main-content">
+			</div><!-- end of main-content -->
+			<div class="main-sidebar">
+			</div><!-- end of main-sidebar -->
+		</main>
+		<footer>
+			
+		</footer>
+	</div><!-- end of wrapper -->
+	
+	<script src="js/menu.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ src/views/_primary-navigation.php
@@ -0,0 +1,59 @@
+<nav class="primary-navigation">
+			<a class="menu-toggle">
+				<div class="menu-toggle__buttons">
+					<i class="fa fa-bars menu-toggle__buttons-open"></i>
+					<i class="fa fa-times-circle menu-toggle__buttons-close" style="display:none;"></i>
+				</div>
+				Menu
+			</a>
+			<div class="primary-navigation__container">
+				<ul class="primary-navigation__menu">
+					<li class="primary-navigation__menu-item">
+						<a href="#" class="navigation-current-page">
+							Home
+							<span>|</span>
+						</a>
+					</li>
+					<li class="primary-navigation__menu-item">
+						<a href="#">
+							K-Beauty
+							<span>|</span>
+						</a>
+					</li>
+					<li class="primary-navigation__menu-item">
+						<a href="#">
+							K-Dramas
+						</a>
+					</li>
+					<li class="primary-navigation__menu-item">
+						<div class="primary-navigation__search">
+							<input type="text" placeholder="Search" name="search" style="display:none;" />
+							<a id="Nav-Search"><i class="fa fa-search"></i></a>
+						</div>
+					</li>
+				</ul>
+				<ul class="primary-navigation__mobile-menu"  style="display: none;">
+					<li class="primary-navigation__mobile-menu-item">
+						<a href="#" class="navigation-current-page">
+							Home
+						</a>
+					</li>
+					<li class="primary-navigation__mobile-menu-item">
+						<a href="#">
+							K-Beauty
+						</a>
+					</li>
+					<li class="primary-navigation__mobile-menu-item">
+						<a href="#">
+							K-Dramas
+						</a>
+					</li>
+					<li class="primary-navigation__mobile-menu-item">
+						<div class="primary-navigation__search">
+							<input type="text" placeholder="Search" name="search" style="display:none;" />
+							<a id="Nav-Search"><i class="fa fa-search"></i></a>
+						</div>
+					</li>
+				</ul>
+			</div><!-- end of main-navigation__container -->
+		</nav>
